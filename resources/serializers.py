2928decc068585db0970da7df0f87878df9f0349
DataDetactive <karimelgohary@ci.suez.edu.eg>
2021-03-30 17:28:30 +0200

    fix theserializer to show all content

--- resources/serializers.py
+++ resources/serializers.py
@@ -1,11 +1,36 @@
 from rest_framework import serializers
-from .models import Movies
+from .models import Movies,Casts,Genres,Moods
+
+
+
+
+
+class CastSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Casts
+        fields = '__all__'
+
+class GenresSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Genres
+        fields = '__all__'
+
+class MoodsSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Moods
+        fields = '__all__'
+        
 
 class MovieSerializer(serializers.ModelSerializer):
+    
     class Meta():
         model = Movies
         #fields = ['name','description','time','image','year','likes','dislikes','url','age','views','trailer','casts','moods','genres']
         fields = '__all__'
+        
+    casts = CastSerializer(read_only=True, many=True)
+    genres = GenresSerializer(read_only=True, many=True)
+    moods = MoodsSerializer(read_only=True,many=True)
 
 
 
