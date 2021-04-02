f977d381e62e5bbff57185df4e030a5b8ca2074a
kareemzak <kareemahmed96@outlook.com>
2021-04-02 22:57:20 +0200

    implemented profile model and views

--- accounts/models/profile.py
+++ accounts/models/profile.py
@@ -1,5 +1,12 @@
 from django.db import models
+from . import Account
 
 
 class Profile(models.Model):
-    pass
+    name = models.CharField(max_length=50, default=f"Profile_{id}")
+    pin_code = models.CharField(max_length=5)
+    image = models.ImageField(upload_to="/")
+    account_id = models.ForeignKey(Account, related_name='profiles', on_delete=models.SET_NULL())
+
+    def __str__(self):
+        return str(f"{self.account_id}_{self.name}")
--- accounts/views/profile.py
+++ accounts/views/profile.py
@@ -2,20 +2,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import serializers
+from accounts.models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Profile
+        fields = ['account_id', 'id', 'name']
 
 
 class ProfileList(ListAPIView):
-    pass
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
 class ProfileCreate(CreateAPIView):
-    pass
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
 class ProfileRUD(RetrieveUpdateDestroyAPIView):
-    pass
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
