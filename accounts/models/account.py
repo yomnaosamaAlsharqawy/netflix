a05d6a0ea8b3563afac8fca10e540b0c266cbda6
mushihata <mushihata@gmail.com>
2021-04-03 02:49:57 +0200

    Initial commit for account model, serializer, and API Views for testing

--- accounts/models/account.py
+++ accounts/models/account.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import User
+from django.db import models
+from . import Plan
+# from . import PaymentMethod
 
 
 class Account(User):
-    pass
+    email = models.EmailField(primary_key=True, unique=True, null=False, balnk=False)
+    password = models.CharField(max_length=100, null=False, blank=False)
+    plan_id = models.ForeignKey(Plan, on_delete=models.SET_NULL, related_name='users')
+    # payment_method = models.ForeignKey(PaymentMethod, on_delete=models.SET_NULL, null=True, blank=True)
+    phone_number = models.CharField(max_length=25, null=True, blank=True)
+    registration_state = models.CharField(choices=['step1', 'step2', 'step3', 'completed'], default='step1', null=False)
+
+    def __str__(self):
+        return self.email
--- accounts/views/account.py
+++ accounts/views/account.py
@@ -1,25 +1,62 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView
 from rest_framework import serializers
+from rest_framework import status
+from accounts.models import Account
+from django.core.validators import validate_email
 
 
-@api_view(["GET", ])
-def test(request):
-    return Response({"message": "It's working"})
+@api_view(['POST', ])
+def email_check(request):
+    try:
+        email = request.data.get('email', None)
+        if email is None or not validate_email(email):
+            return Response({'error': 'A valid email must be provided!'}, status=status.HTTP_400_BAD_REQUEST)
+        Account.objects.get(pk=email)
+    except ObjectDoesNotExist:
+        return Response({'exists': False}, status=status.HTTP_404_NOT_FOUND)
+
+    return Response({'exists': True}, status=status.HTTP_200_OK)
 
 
 class AccountSerializer(serializers.ModelSerializer):
-    pass
+    password = serializers.CharField(write_only=True)
+    password1 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Account
+        exclude = ('password', )
+
+    def create(self, validated_data):
+        email = validated_data.get('email', None)
+        password = validated_data.get('password', None)
+        password1 = validated_data.get('password', None)
+
+        if password is None:
+            raise serializers.ValidationError({
+                'password': 'Password must be provided'
+            })
+
+        if password1 is None or password != password1:
+            raise serializers.ValidationError({
+                'password1': "Passwords don't match"
+            })
+
+        if email is None:
+            raise serializers.ValidationError({
+                'email': 'A valid email must be provided'
+            })
+
+        return Account.objects.create(email=email, password=password)
 
 
 class AccountCreate(CreateAPIView):
-    pass
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
 
 
 class AccountRetrieveUpdate(RetrieveUpdateAPIView):
-    pass
-
-
-class AccountCheck(RetrieveAPIView):
-    pass
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
