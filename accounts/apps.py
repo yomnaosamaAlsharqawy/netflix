8fe099023701c21d7fadfa19fc411d242b2fc18c
yomnaosamaAlsharqawy <53663886+yomnaosamaAlsharqawy@users.noreply.github.com>
2021-04-11 00:43:44 +0200

    signals of plans

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class AccountsConfig(AppConfig):
@@ -6,3 +7,4 @@ class AccountsConfig(AppConfig):
 
     def ready(self):
         from . import signals
+        post_migrate.connect(signals.init_plans, sender=self)
